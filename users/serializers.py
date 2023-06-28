68d76ddb04c0adfb3ef6f038b30b9a3cfe9684dc
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-06-28 16:13:42 +0300

    [add] users: viewset

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,13 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
