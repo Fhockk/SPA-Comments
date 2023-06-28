210891ca419c03bb76e5018faf202ac4c78fe8bc
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-06-28 12:28:29 +0300

    [add] users, comments: serializers

---
+++ comments/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from comments.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    replies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = (
+        'id', 'author', 'home_page', 'content', 'image', 'text_file', 'created_at', 'updated_at', 'parent', 'replies'
+        )
+
+    def get_replies(self, obj):
+        if obj.replies:
+            return CommentSerializer(obj.replies.all(), many=True).data
+        return None
