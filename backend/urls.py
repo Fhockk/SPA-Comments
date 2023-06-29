26dc5d6c342f1608912145e12a14fae9c78f6831
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-06-29 21:37:54 +0300

    [add] comments: viewset

--- backend/urls.py
+++ backend/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+    path('', include('comments.urls')),
 ]
 
 
