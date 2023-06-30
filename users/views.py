2dd67eb0ae377d02e70abbc02dfa55bbe9b9957c
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-06-30 19:18:10 +0300

    [add] users: cache (redis)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from rest_framework import viewsets
 
 from users.models import User
@@ -7,3 +9,11 @@ from users.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @method_decorator(cache_page(60 * 60 * 1))  # кеширование на 1 час
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 60 * 1))  # кеширование на 1 час
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
