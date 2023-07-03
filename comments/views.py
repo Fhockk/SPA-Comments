fd80aa06b9ca8d40e24f08c6558007085ea3bdde
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-07-04 00:24:36 +0300

    [add] comments: order by another options

--- comments/views.py
+++ comments/views.py
@@ -1,14 +1,18 @@
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
 from comments.models import Comment
 from comments.serializers import CommentSerializer
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.filter(parent=None).order_by('-created_at').prefetch_related('replies')
+    queryset = Comment.objects.filter(parent=None).prefetch_related('replies')
     serializer_class = CommentSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['created_at', 'updated_at', 'author__username', 'author__email']
+    ordering = ['-created_at']
 
     @method_decorator(cache_page(30))  # кеширование на 1 час 60 * 60 * 1
     def list(self, request, *args, **kwargs):
